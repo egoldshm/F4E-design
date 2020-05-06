3312c6ed442ff552703be74618b93078ca743cf5
Liav Sofer <Liavsofer43@gmail.com>
2020-05-06 16:55:57 +0300

    fix filteringSettings Page

--- F4E-design/Pages/FilterSettingsPage.xaml.cs
+++ F4E-design/Pages/FilterSettingsPage.xaml.cs
@@ -20,9 +20,27 @@ namespace F4E_design.Pages
     /// </summary>
     public partial class FilterSettingsPage : Page
     {
+        private const string URI_ON_IMAGE = "/images/categorizepage/on.png";
+        private const string URI_OFF_IMAGE = "/images/categorizepage/off.png";
+
         public FilterSettingsPage()
         {
             InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Image image = sender as Image;
+            if (image.Name.EndsWith("Active"))
+            {
+                image.Source = new BitmapImage(new Uri(URI_ON_IMAGE, UriKind.Relative));
+                image.Name = image.Name.Replace("Active", "");
+            }
+            else
+            {
+                image.Source = new BitmapImage(new Uri(URI_OFF_IMAGE, UriKind.Relative));
+                image.Name = image.Name != null ? image.Name + "Active" : "Active";
+            }
         }
     }
 }
