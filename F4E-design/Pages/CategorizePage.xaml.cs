1751150fbb1a217640681c5a4c93f33906858eb4
egoldshm <39948614+egoldshm@users.noreply.github.com>
2020-05-06 16:43:09 +0300

    fixed issus and added switch button in catgory

--- F4E-design/Pages/CategorizePage.xaml.cs
+++ F4E-design/Pages/CategorizePage.xaml.cs
@@ -21,10 +21,28 @@ namespace F4E_design.Pages
     /// </summary>
     public partial class CategorizePage : Page
     {
+        //(const = readonly static)
+        private const string URI_ON_IMAGE = "/images/categorizepage/on.png";
+        private const string URI_OFF_IMAGE = "/images/categorizepage/off.png";
+
         public CategorizePage()
         {
-            
+            InitializeComponent();
         }
-        
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Image image = sender as Image;
+            if (image.Name.EndsWith("Active"))
+            {
+                image.Source = new BitmapImage(new Uri(URI_ON_IMAGE, UriKind.Relative));
+                image.Name = image.Name.Replace("Active", "");
+            }
+            else
+            {
+                image.Source = new BitmapImage(new Uri(URI_OFF_IMAGE, UriKind.Relative));
+                image.Name = image.Name != null ? image.Name + "Active" : "Active";
+            }
+        }
     }
 }
