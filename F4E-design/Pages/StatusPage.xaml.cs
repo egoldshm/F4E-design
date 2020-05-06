a355ff0fae0603720f295e9be99a2fb79c45493f
Liav Sofer <Liavsofer43@gmail.com>
2020-05-06 17:12:53 +0300

    fix statusPage

--- F4E-design/Pages/StatusPage.xaml.cs
+++ F4E-design/Pages/StatusPage.xaml.cs
@@ -30,5 +30,36 @@ namespace F4E_design
         {
             Image image = sender as Image;
         }
+        private void sceduelToggle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            string URI_ON_IMAGE = "/images/statusPage/scheduel_on.png";
+            const string URI_OFF_IMAGE = "/images/statusPage/scheduel_off.png";
+            if (scheduelToggle.Name.EndsWith("Active"))
+            {
+                scheduelToggle.Source = new BitmapImage(new Uri(URI_ON_IMAGE, UriKind.Relative));
+                scheduelToggle.Name = scheduelToggle.Name.Replace("Active", "");
+            }
+            else
+            {
+                scheduelToggle.Source = new BitmapImage(new Uri(URI_OFF_IMAGE, UriKind.Relative));
+                scheduelToggle.Name = scheduelToggle.Name != null ? scheduelToggle.Name + "Active" : "Active";
+            }
+        }
+
+        private void systemStatusToggle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            string URI_ON_IMAGE = "/images/statusPage/status_on.png";
+            const string URI_OFF_IMAGE = "/images/statusPage/status_off.png";
+            if (systemStatusToggle.Name.EndsWith("Active"))
+            {
+                systemStatusToggle.Source = new BitmapImage(new Uri(URI_ON_IMAGE, UriKind.Relative));
+                systemStatusToggle.Name = scheduelToggle.Name.Replace("Active", "");
+            }
+            else
+            {
+                systemStatusToggle.Source = new BitmapImage(new Uri(URI_OFF_IMAGE, UriKind.Relative));
+                systemStatusToggle.Name = scheduelToggle.Name != null ? scheduelToggle.Name + "Active" : "Active";
+            }
+        }
     }
 }
